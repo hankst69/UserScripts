8175b389856086077b0e7c83f08ece70f3ebecc6
Steffen Hanke <steffen.hanke.ext@siemens-healthineers.com>
2025-04-07 21:20:52 +0200

    updated ImproveWebReadability 0.1.3

--- ImproveWebReadability/IWRDTY.user.js
+++ ImproveWebReadability/IWRDTY.user.js
@@ -2,51 +2,54 @@
 // "https://*.de/*"
 // "https://www.klettern.de/*"
 // "https://www.rockandice.com/*"
+// "https://www.adac.de/*"
 
 (function content() {
 
     //--------------------------------------------------------------------------------------------------
     // basic script functions
 
-    var alertMode = false;
-    var verboseMode = false; //true;
+    let alertMode = false;
+    let verboseMode = true;
+    let appendToDom = false;
+    let DEBUG_ID = "DEBUG_IWRDTY";
 
-    function debug(doAlert, str) {
-      if (doAlert && alertMode) {
-        alert(str);
+    function debug(vipLevel, msg) {
+      if (vipLevel && alertMode) {
+        alert(msg);
       }
-      if (doAlert || verboseMode) {
-        console.log(str);
+      if (vipLevel || verboseMode) {
+        console.log("[Improve Web Readability] " + msg);
+        if (appendToDom) {
+          let debugElem = document.getElementById(DEBUG_ID);
+          if (!debugElem) {
+            debugElem = createHiddenElem('div', DEBUG_ID);
+          }
+          debugElem.appendChild(document.createTextNode(msg + ' '));
+        }
       }
-      //var debugElem=document.getElementById(DEBUG_ID);
-      //if (!debugElem) {
-      //  debugElem=createHiddenElem('div', DEBUG_ID);
-      //}
-      //debugElem.appendChild(document.createTextNode(str+' '));
     }
 
-    var domLoaded = function (callback) {
-        /* Internet Explorer */
-        /*
-        @cc_on
-        @if (@_win32 || @_win64)
-          document.write('<script id="ieScriptLoad" defer src="//:"><\/script>');
-          document.getElementById('ieScriptLoad').onreadystatechange = function () {
-            if (this.readyState == 'complete') {
-              callback();
-            }
-          };
-          return;
-        @end@
-        */
-        /* Mozilla, Chrome, Opera */
+    let domLoaded = function (callback) {
+        //* Internet Explorer *
+        //@cc_on
+        //@if (@_win32 || @_win64)
+        //  document.write('<script id="ieScriptLoad" defer src="//:"><\/script>');
+        //  document.getElementById('ieScriptLoad').onreadystatechange = function () {
+        //    if (this.readyState == 'complete') {
+        //      callback();
+        //    }
+        //  };
+        //  return;
+        //@end@
+        //* Mozilla, Chrome, Opera *
         if (document.addEventListener) {
             document.addEventListener('DOMContentLoaded', callback, false);
             return;
         }
-        /* Safari, iCab, Konqueror */
+        //* Safari, iCab, Konqueror *
         if (/KHTML|WebKit|iCab/i.test(navigator.userAgent)) {
-            var DOMLoadTimer = setInterval(function () {
+            let DOMLoadTimer = setInterval(function () {
                 if (/loaded|complete/i.test(document.readyState)) {
                     callback();
                     clearInterval(DOMLoadTimer);
@@ -54,7 +57,7 @@
             }, 10);
             return;
         }
-        /* Other web browsers */
+        //* Other web browsers *
         window.onload = callback;
     };
 
@@ -132,7 +135,7 @@
 
     function processStart() {
         debug(false, "processStart");
-        var div = document.getElementById(MASK_ID_PROCESSING);
+        let div = document.getElementById(MASK_ID_PROCESSING);
         debug(false, "processStart:: found div MASK_ID_PROCESSING : " + div);
 
         if (!div) {
@@ -248,6 +251,7 @@
     //--------------------------------------------------------------------------------------------------
 
 
+
     //----------------------------------------------------------------------------------------------------
     // START of main routine (IWRDTYmodifyDocument)
 
@@ -265,34 +269,41 @@
         }
 
         // decide page type
-        var isAnyOfValidPages = false;
-        var isKletternDePage = false;
-        var isHeiseDePage = false;
-        var isGigaDePage = false;
-        var isRockAndIceComPage = false;
+        let isAnyOfValidPages = false;
+        let isKletternDePage = false;
+        let isHeiseDePage = false;
+        let isGigaDePage = false;
+        let isAdacDePage = false;
+        let isRockAndIceComPage = false;
         if (!isAnyOfValidPages) {
-          if (document.URL.indexOf('klettern.de') > 0) {
-            isKletternDePage = true; isAnyOfValidPages = true;
-            debug(true, "IWRDTYmodifyDocument -> isKletternDePage");
-          }
+            if (document.URL.indexOf('.klettern.de') > 0) {
+                isKletternDePage = true; isAnyOfValidPages = true;
+                debug(true, "IWRDTYmodifyDocument -> isKletternDePage");
+            }
         }
         if (!isAnyOfValidPages) {
-          if (document.URL.indexOf('heise.de') > 0) {
-            isHeiseDePage = true; isAnyOfValidPages = true;
-            debug(true, "IWRDTYmodifyDocument -> isHeiseDePage");
-          }
+            if (document.URL.indexOf('.adac.de') > 0) {
+                isAdacDePage = true; isAnyOfValidPages = true;
+                debug(true, "IWRDTYmodifyDocument -> isAdacDePage");
+            }
         }
         if (!isAnyOfValidPages) {
-          if (document.URL.indexOf('giga.de') > 0) {
-            isGigaDePage = true; isAnyOfValidPages = true;
-            debug(true, "IWRDTYmodifyDocument -> isGigaDePage");
-          }
+            if (document.URL.indexOf('.heise.de') > 0) {
+                isHeiseDePage = true; isAnyOfValidPages = true;
+                debug(true, "IWRDTYmodifyDocument -> isHeiseDePage");
+            }
         }
         if (!isAnyOfValidPages) {
-          if (document.URL.indexOf('rockandice.com') > 0) {
-            isRockAndIceComPage = true; isAnyOfValidPages = true;
-            debug(true, "IWRDTYmodifyDocument -> isRockAndIceComPage");
-          }
+            if (document.URL.indexOf('.giga.de') > 0) {
+                isGigaDePage = true; isAnyOfValidPages = true;
+                debug(true, "IWRDTYmodifyDocument -> isGigaDePage");
+            }
+        }
+        if (!isAnyOfValidPages) {
+            if (document.URL.indexOf('.rockandice.com') > 0) {
+                isRockAndIceComPage = true; isAnyOfValidPages = true;
+                debug(true, "IWRDTYmodifyDocument -> isRockAndIceComPage");
+            }
         }
         //if (!isAnyOfValidPages) {
         //  Array.prototype.forEach.call(document.documentElement.querySelectorAll("div#content>div.columns>div#content-center>div#current-topo-wrapper"), function (element) {
@@ -303,113 +314,115 @@
 
         // do not process specific page types
         if (!isAnyOfValidPages) {
-          return;
+            return;
         }
 
         debug(true, "IWRDTYmodifyDocument:: start Processing");
-
         processStart();
-
-        var dochtml = document.documentElement;
+        let dochtml = document.documentElement;
 
         // (1) GENRAL processing
         removeGoogleAnalytics(dochtml);
-
-        //var allChilds = getChildTreeAsNodeList(dochtml); //debug(false, "IWRDTYmodifyGebietPage getChildTreeAsNodeList.length: " + allChilds.length);
+        //let allChilds = getChildTreeAsNodeList(dochtml); //debug(false, "IWRDTYmodifyGebietPage getChildTreeAsNodeList.length: " + allChilds.length);
         //for (var child of allChilds) {
         //  if (child.tagName == "H3") {
         //    //child.parentElement.removeChild(child);
         //  }
         //}
 
-        // (4) remove general unwanted content
 
+        // (2) remove specific unwanted content
+        if (isAdacDePage) {
+            debug(false, "isAdacDePage")
+            Array.prototype.forEach.call(dochtml.querySelectorAll("main>div+div"), function (element) {
+                debug(false, "found div")
+                //element.className = ""
+                element.style.maxWidth = ""
+            });
+            Array.prototype.forEach.call(dochtml.querySelectorAll("main"), function (element) {
+                debug(false, "found main")
+                element.className = ""
+            });
+        }
         if (isKletternDePage) {
-          //removeElement(dochtml, "div.v-A_-wrapper--stroer");
-          removeElement(dochtml, "aside.mps-aside");
-          removeElement(dochtml, "nav.mps-article-topics");
-          removeElement(dochtml, "section.mps-partner");
-          removeElement(dochtml, "div#topBanner");
-          removeElement(dochtml, "div#rightBanner");
-          removeElement(dochtml, "div#taboola-below-article-thumbnails");
-          removeElement(dochtml, "div#div-gpt-ad-pubperform");
-          removeElement(dochtml, "div#div-gpt-ad-idx_con_oben");
-          removeElement(dochtml, "div#div-gpt-ad-art_con_oben");
-          removeElement(dochtml, "div#div-gpt-ad-art_con_mitte");
-          removeElement(dochtml, "div#div-gpt-ad-art_pic");
-          removeElement(dochtml, "div#div-gpt-ad-banner");
-          removeElement(dochtml, "div#div-gpt-ad-sky");
-          removeElement(dochtml, "div#div-gpt-ad-posterad");
-          removeElement(dochtml, "div#div-gpt-ad-rectangle");
-          removeElement(dochtml, "div.sm-screen"); //"teads-inread sm-screen"
-          removeElement(dochtml, "footer.mps-footer");
-          removeElement(dochtml, "div.scroller");
-          // hide Inhaltsverzeichnis oben:
-          hideElement(dochtml, "div#pagination-content-top");
-          // hide Photoshow Boxes:
-          hideElement(dochtml, "div.photo-show"); //"mps-ce box photo-show"
+            removeElement(dochtml, "aside.mps-aside");
+            removeElement(dochtml, "nav.mps-article-topics");
+            removeElement(dochtml, "section.mps-partner");
+            removeElement(dochtml, "div#topBanner");
+            removeElement(dochtml, "div#rightBanner");
+            removeElement(dochtml, "div#taboola-below-article-thumbnails");
+            removeElement(dochtml, "div#div-gpt-ad-pubperform");
+            removeElement(dochtml, "div#div-gpt-ad-idx_con_oben");
+            removeElement(dochtml, "div#div-gpt-ad-art_con_oben");
+            removeElement(dochtml, "div#div-gpt-ad-art_con_mitte");
+            removeElement(dochtml, "div#div-gpt-ad-art_pic");
+            removeElement(dochtml, "div#div-gpt-ad-banner");
+            removeElement(dochtml, "div#div-gpt-ad-sky");
+            removeElement(dochtml, "div#div-gpt-ad-posterad");
+            removeElement(dochtml, "div#div-gpt-ad-rectangle");
+            removeElement(dochtml, "div.sm-screen"); //"teads-inread sm-screen"
+            removeElement(dochtml, "footer.mps-footer");
+            removeElement(dochtml, "div.scroller");
+            // hide Inhaltsverzeichnis oben:
+            hideElement(dochtml, "div#pagination-content-top");
+            // hide Photoshow Boxes:
+            hideElement(dochtml, "div.photo-show"); //"mps-ce box photo-show"
         }
         if (isHeiseDePage) { 
-          removeElement(dochtml, "a-ad");
-          removeElement(dochtml, "div#wtma_teaser");
-          removeElement(dochtml, "div#mitte_rechts");
-          removeElement(dochtml, "aside.akwa-ad-container");
-          removeElement(dochtml, "aside.recommendations");
-          removeElement(dochtml, "div.teads-inread");
-          //
-          removeElement(dochtml, "div.ho-stage-container");
-          removeElement(dochtml, "div.bottom_up");
-          removeElement(dochtml, "footer.main-footer");
+            removeElement(dochtml, "a-ad");
+            removeElement(dochtml, "div#wtma_teaser");
+            removeElement(dochtml, "div#mitte_rechts");
+            removeElement(dochtml, "aside.akwa-ad-container");
+            removeElement(dochtml, "aside.recommendations");
+            removeElement(dochtml, "div.teads-inread");
+            removeElement(dochtml, "div.ho-stage-container");
+            removeElement(dochtml, "div.bottom_up");
+            removeElement(dochtml, "footer.main-footer");
         }
         if (isGigaDePage) { 
-          removeElement(dochtml, "div.u-shape");
-          removeElement(dochtml, "div.u-shape-top");
-          removeElement(dochtml, "div.section"); //section spacer-md
-          removeElement(dochtml, "div.ed-container");
-          removeElement(dochtml, "div.social-media-bar");
-          removeElement(dochtml, "footer.main-footer");
-          
-          removeElement(dochtml, "div#sdgAdServerContainer-posterad");
-          removeElement(dochtml, "div#sdgAdServerContainer-banner");
-          removeElement(dochtml, "div#sdgAdServerContainer-sky");
+            removeElement(dochtml, "div.u-shape");
+            removeElement(dochtml, "div.u-shape-top");
+            removeElement(dochtml, "div.section"); //section spacer-md
+            removeElement(dochtml, "div.ed-container");
+            removeElement(dochtml, "div.social-media-bar");
+            removeElement(dochtml, "footer.main-footer");
+            removeElement(dochtml, "div#sdgAdServerContainer-posterad");
+            removeElement(dochtml, "div#sdgAdServerContainer-banner");
+            removeElement(dochtml, "div#sdgAdServerContainer-sky");
         }
         if (isRockAndIceComPage) { 
-          removeElement(dochtml, "div.topAdsBanner");
-          removeElement(dochtml, "div.googleAdds");
-          removeElement(dochtml, "div.minibanner");
-          removeElement(dochtml, "section.boxed");
-          removeElement(dochtml, "footer.site-footer");
-          //removeElement(dochtml, "div.row"); // removes News from right side
-          removeElement(dochtml, "div.dynamicoffset"); // removes News from right side and bottom
-
-          Array.prototype.forEach.call(dochtml.querySelectorAll("div.content-area"), function (element) {
-              element.style.width = '90%';
-          });
-          Array.prototype.forEach.call(dochtml.querySelectorAll("div.col-md-10"), function (element) {
-              element.style.width = '100%';
-          });
+            removeElement(dochtml, "div.topAdsBanner");
+            removeElement(dochtml, "div.googleAdds");
+            removeElement(dochtml, "div.minibanner");
+            removeElement(dochtml, "section.boxed");
+            removeElement(dochtml, "footer.site-footer");
+            // removeElement(dochtml, "div.row"); // removes News from right side
+            removeElement(dochtml, "div.dynamicoffset"); // removes News from right side and bottom
+            Array.prototype.forEach.call(dochtml.querySelectorAll("div.content-area"), function (element) {
+                element.style.width = '90%';
+            });
+            Array.prototype.forEach.call(dochtml.querySelectorAll("div.col-md-10"), function (element) {
+                element.style.width = '100%';
+            });
         }
 
         debug(true, "IWRDTYmodifyDocument:: finished Processing");
         processEnd();
     }
     
+
     //----------------------------------------------------------------------------------------------------
     // START of document processing
-
     debug(true, "START of document processing");
-
-    //var topWindow = window == top;
+    //let topWindow = window == top;
     //debug(false, "this.url: " + this.url);
     //debug(false, "document.URL: " + document.URL);
-
     // here we do things after DOM loaded (e.g. kickout scripts we do not want to run or modify configurations)
     // for our scenario the current implementation is to early (with CHROME)
     //domLoaded(function () {
     //    debug(true, "domLoaded");
     //    IWRDTYmodifyDocument(document);
     //});
-
     // here we do further processings (to get all in a single file) after document completely loaded (and modified)
     window.onload = function () {
         debug(false, "onload");
